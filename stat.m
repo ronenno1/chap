dc93988aee94c121bb96693faf2af880b69142e5
ronenno1 <ronen.no1@gmail.com>
2018-11-16 09:40:52 +0200

    add support with Holm-Bonferroni correction

--- stat.m
+++ stat.m
@@ -16,33 +16,54 @@ classdef stat
             statistical_data = stat.do_stat(total_data, comp_names, configuration, statistical_data);
             statistical_data.configuration = configuration;
             stat.plot_statistics(0,0, fig, figure, statistical_data, total_data, 1, 0, next, back);
-        end;
+        end
 
         function plot_statistics(src, evnt, fig, figure, statistical_data, total_data, contrast_id, action, next, back)
             statistical_data.configuration = stat.statistical_vals(statistical_data.configuration);
             bayesian = strcmp(statistical_data.configuration.vals.approach, 'Bayesian');
+            descriptive = strcmp(statistical_data.configuration.vals.output_type, 'descriptive');
+
             approach_str = 'BFs';
+
             if ~bayesian
-                approach_str = 'tValues';
-            end;
+                approach_str = 'pValues';
+            end
             contrast_names = fieldnames(statistical_data.contrasts.(approach_str));
             header = statistical_data.contrast_names;
             
             if (contrast_id+action)>size(contrast_names, 1) || contrast_id+action<1
                 return;
-            end;
-
-            contrast_data  = statistical_data.contrasts.(approach_str).(char(contrast_names(contrast_id+action)));
+            end
+            
+            bayesian_contrast_data       = statistical_data.contrasts.BFs.(char(contrast_names(contrast_id+action)));
+            classical_contrast_data_bonf = statistical_data.contrasts.pValues_bonf.(char(contrast_names(contrast_id+action)));
+            contrast_data                = bayesian_contrast_data;
+            
+            if ~bayesian
+                contrast_data = classical_contrast_data_bonf;
+            end
+            
+            
             contrast_data_effect  = statistical_data.contrasts.pes.(char(contrast_names(contrast_id+action)));
           
             x_axis = statistical_data.configuration.x_axis;
             if (size(statistical_data.configuration.x_axis, 2)> size(contrast_data, 2))
                 x_axis = x_axis(1:size(contrast_data, 2));
-            end;
+            end
             data2plot = contrast_data(1:size(x_axis, 2));
             max_bf = max(data2plot);
-            data2plot(data2plot<3) = nan;
-            data2plot(data2plot>=3) = -max_bf/50;
+            if bayesian
+                data2plot(data2plot<3) = nan;
+                data2plot(data2plot>=3) = -max_bf/50;
+            else
+                data2plot_001(data2plot>.001) = nan;
+                data2plot_001(data2plot<.001) = -max_bf/50;
+                data2plot_01(find(data2plot>.01 | data2plot<.001)) = nan;
+                data2plot_01(find(data2plot<.01 & data2plot>=.001)) = -max_bf/50;
+                
+                data2plot_05(find(data2plot>.05 | data2plot<.01)) = nan;
+                data2plot_05(find(data2plot<.05 & data2plot>=.01)) = -max_bf/50;
+            end
             
             data2plot_ns = contrast_data(1:size(x_axis, 2));
             data2plot_ns(data2plot_ns>(1/3)) = nan;
@@ -59,18 +80,26 @@ classdef stat
                 
                 plot(fig, x_axis, data2plot(1:size(x_axis, 2)), 'black', 'LineWidth', 0.1, 'Marker','s', 'LineStyle','none',  'MarkerFaceColor', 'black');
                 plot(fig, x_axis, data2plot_ns(1:size(x_axis, 2)), 'green', 'LineWidth', 0.1, 'Marker','s', 'LineStyle','none',  'MarkerFaceColor', 'green');
-                
                 ylim([-inf inf])
-            end;
+            else
+                plot(fig, x_axis, data2plot_001(1:size(x_axis, 2)), 'color', [0, 0, 0], 'LineWidth', 0.1, 'Marker','s', 'LineStyle','none',  'MarkerFaceColor', [0, 0, 0]);
+                plot(fig, x_axis, data2plot_01(1:size(x_axis, 2)), 'color', [.3, .3, .3], 'LineWidth', 0.1, 'Marker','s', 'LineStyle','none',  'MarkerFaceColor', [.3, .3, .3]);
+                plot(fig, x_axis, data2plot_05(1:size(x_axis, 2)), 'color', [.6, .6, .6], 'LineWidth', 0.1, 'Marker','s', 'LineStyle','none',  'MarkerFaceColor', [.6, .6, .6]);
+
+            end
             set(fig,'FontWeight','bold');
 
             xlabel(fig, 'Time [ms]', 'FontWeight','bold');
             
-            ylabel(fig, 't-value', 'FontWeight','bold');
+
             if bayesian
                 ylabel(fig, 'BF', 'FontWeight','bold');
                 legend(fig, ['BF_{10}'; 'BF_{01}']);
-            end;
+            else
+                ylabel(fig, 'p-value', 'FontWeight','bold');
+                legend(fig, 'p-value');
+
+            end
             xlim(fig, [statistical_data.configuration.x_axis(1) statistical_data.configuration.x_axis(end)])
 
             title(fig, header(contrast_id+action));        
@@ -78,10 +107,10 @@ classdef stat
             set(back, 'Visible', 'off');
             if (contrast_id+action)<size(contrast_names, 1)
                 set(next, 'Visible', 'on');
-            end;
+            end
             if contrast_id+action>1
                 set(back, 'Visible', 'on');
-            end;
+            end
             set(next, 'callback', {@stat.plot_statistics, fig, figure, statistical_data, total_data, contrast_id+action, 1, next, back}); 
             set(back, 'callback', {@stat.plot_statistics, fig, figure, statistical_data, total_data, contrast_id+action, -1, next, back}); 
             width = 822;
@@ -103,7 +132,7 @@ classdef stat
                 t = statistical_data.contrasts.total.t(i);
                 N = statistical_data.contrasts.total.N(i);
                 data_table(i,:) = [cellstr(comparison), t, BF_10, 1./BF_10, cohensd, N];
-            end;
+            end
             
             uitable(hpt, 'Data', data_table, 'position', [x y width-1 hight-1],...
                           'ColumnName', header2', 'ColumnWidth',{500,70, 50, 50, 70, 40}); 
@@ -116,7 +145,7 @@ classdef stat
         
 
             end
-            if strcmp(statistical_data.configuration.vals.output_type, 'descriptive')
+            if descriptive
                 data_table = table2cell(statistical_data.output.descriptive);
                 header2 = statistical_data.output.descriptive.Properties.VariableNames;
                 uitable(hpt, 'Data', data_table, 'position', [x y width-1 hight-1],...
@@ -142,7 +171,7 @@ classdef stat
             path2save = strcat(paths.stat_output_folder_name, filesep, folder_name);
              if ~exist(path2save, 'dir')
                 mkdir(path2save);
-            end;
+            end
 
             writetable(output.descriptive, strcat(path2save, filesep, 'descriptive.csv'));
             writetable(output.inference, strcat(path2save, filesep, 'inference.csv'));
@@ -150,7 +179,7 @@ classdef stat
             writetable(output.bayesian_sequential, strcat(path2save, filesep, 'bayesian_sequential.csv'));
             writetable(output.raw_data, strcat(path2save, filesep, 'avgs_table.csv'));
 
-        end;
+        end
         
         function [t, sd, N, v] = my_ttest(data1, data2)
            data = data1- data2; 
@@ -162,12 +191,13 @@ classdef stat
            v = N-1;         
         end
         
-        function [t, bf, N, sd, pes] = ttest_and_bf(data1, data2)
+        function [t, bf, N, sd, pes, p_value] = ttest_and_bf(data1, data2)
             [t, sd, N, v] = stat.my_ttest(data1, data2);
             if t == Inf || isnan(t)
                 t = NaN;
                 bf = NaN;
                 pes = NaN;
+                p_value = NaN;
                 return;
             end
             r   = 0.707;
@@ -177,10 +207,12 @@ classdef stat
             
             bf = b/a;
             pes = t^2/(t^2 + v);
-        end;
+            p_value = 1-fcdf(t^2, 1, v);
+            
+        end
         
         function num = rounder(X, N)
-            if (~exist('N', 'var'))
+        if (~exist('N', 'var'))
                 N = 3;
             end
             num = round(X*(10^N))/10^N;
@@ -216,7 +248,7 @@ classdef stat
             stat_data.configuration.x_axis = total_data.x_axis;
             if (~exist('statistical_data', 'var'))
                 statistical_data = [];
-            end;
+            end
             if (~exist('configuration', 'var'))
                 from = total_data.x_axis(1);
                 to = total_data.x_axis(end);
@@ -229,24 +261,24 @@ classdef stat
                 measure = configuration.vals.measure;
                 approach = configuration.vals.approach;
                 output_type = configuration.vals.output_type;
-            end;
+            end
             bayesian = strcmp(approach, 'Bayesian');
 
             min_length = inf;
             for comp = 1:size(comp_names, 1)
                 min_length = min(min_length, size(total_data.(char(comp_names(comp))).data, 2));
-            end;
+            end
 
             contrasts = nchoosek(1:size(comp_names, 1), 2);
             
             from_ana = 1; 
             if from > total_data.x_axis(1)
                 from_ana = find(total_data.x_axis<=from, 1, 'last');
-            end;
+            end
             to_ana = length(total_data.x_axis)-1; 
             if to < total_data.x_axis(end)
                 to_ana = find(total_data.x_axis>=to, 1, 'first')-1;
-            end;
+            end
             
 
             for comp = 1:size(comp_names, 1)
@@ -259,7 +291,7 @@ classdef stat
                         avgs.(char(comp_names(comp))) = val';
                     else
                         avgs.(char(comp_names(comp))) = configuration.x_axis(from_ana+pos)';
-                    end;
+                    end
                 elseif strcmp(measure,'dip') || strcmp(measure,'dip latency')
                     [val, pos]= nanmin(full_data(:, from_ana:to_ana)');
 
@@ -267,9 +299,9 @@ classdef stat
                         avgs.(char(comp_names(comp))) = val';
                     else
                         avgs.(char(comp_names(comp))) = configuration.x_axis(from_ana+pos)';
-                    end;
-                end;
-            end;
+                    end
+                end
+            end
 
             avgs_table = struct2table(avgs);
             avgs_table_mat = avgs_table{:,1:end};
@@ -278,7 +310,7 @@ classdef stat
             num_of_conditions = size(avgs_table_mat, 2);
             if(num_of_subjects<2 || num_of_conditions<2)
                 return;
-            end;
+            end
 
             comp_names_fixed = cellfun(@(x) x(3:end), comp_names, 'UniformOutput', false);
             comp_names_fixed = strrep(strrep(comp_names_fixed, '_x_', ' & '),'_',' ');
@@ -315,9 +347,9 @@ classdef stat
                            comps_t(i, j-1) = i-1;
                         elseif i>j
                             comps_t(i, j-1) = 0;
-                        end;
-                    end;
-                end;
+                        end
+                    end
+                end
 
 
                 means_conditions_extended      = zeros(num_of_conditions-1, num_of_conditions);
@@ -334,7 +366,7 @@ classdef stat
                     small_table (:, (comps_t(:, comp)==0)) = [];
                     comp_sse(comp) = stat.calc_ss(small_table);
                     comp_mse(comp) = stat.calc_ss(small_table)/dfe_comp;
-                end;
+                end
 
                 F_comp_all  = (ss_comp_all./comp_mse);
                 p_comp__all  = 1-fcdf(F_comp_all, 1, dfe_comp);
@@ -346,21 +378,21 @@ classdef stat
                         weight = comps_t(pos, contrast);
                         if weight ==0
                             continue;
-                        end;
+                        end
                         if sign(weight)>0
                             name = strcat(name, '+');
                         else
                             name = strcat(name, '-');
-                        end;
+                        end
                         name = strcat(name, num2str(abs(weight)), '*', comp_names_fixed(pos));
-                    end;
+                    end
                     fixed_contrast_names(contrast, :) = name;
                 end
 
                 p_str = stat.rounder(p);
                 if(~p_str)
                     p_str = p;
-                end;
+                end
                 
                 stat_data.output.inference.Comperison = ' ';
                 stat_data.output.inference.dfa = stat.rounder(dfa);
@@ -374,7 +406,7 @@ classdef stat
                 stat_data.output.comps = table(fixed_contrast_names, ones(size(comps_t, 2), 1), dfe*ones(size(comps_t, 2), 1), F_comp_all', comp_mse', p_comp__all', cell(size(comps_t, 2), 1),...
                                                'VariableNames',{'Comperison', 'dfa', 'dfe', 'F', 'MSE', 'Pvalue', 'pes'});
                 stat_data.output.clasical = [struct2table(stat_data.output.inference); stat_data.output.comps];  
-            end;
+            end
 
             stat_data.output.raw_data = avgs_table;
 
@@ -400,30 +432,53 @@ classdef stat
                     for sample = 1:min_length
                         percentage = round(100*((sample+min_length*(contrast-1))/(min_length*size(contrasts, 1))));
                         if percentage>0 && ~mod(percentage, 10) && percentages(percentage/10)==0 
-    %                         ['samples analyzed: ' num2str(percentage) '%']
                             percentages(percentage/10) = 1;
-                        end;
+                        end
 
                         data1 = full_data1(:, sample);
                         data2 = full_data2(:, sample);
-                        [t, bf, ~, ~, pes] = stat.ttest_and_bf(data1, data2);
+                        [t, bf, ~, ~, pes, pvalue] = stat.ttest_and_bf(data1, data2);
                         stat_data.contrasts.tValues.(char(['contrast_' num2str(contrast)]))(:, sample)  = t;
+                        stat_data.contrasts.pValues.(char(['contrast_' num2str(contrast)]))(:, sample)  = pvalue;
                         stat_data.contrasts.BFs.(['contrast_' num2str(contrast)])(:, sample)      = bf;
 
                         mean_diff = nanmean(data1)-nanmean(data2); 
                         mean_std = nanmean(nanstd(data1)^2-nanstd(data2)^2); 
                         stat_data.contrasts.pes.(['contrast_' num2str(contrast)])(:, sample)      = pes;
-                    end;
+                    end
+                    pValues = stat_data.contrasts.pValues.(['contrast_' num2str(contrast)]);
+                    num_of_samples           = length(pValues);
+                    [sorted_data, sort_ids]  = sort(pValues);  
+                    [~, real_ids]            = sort(sort_ids);  
+                    factors                  = num_of_samples:-1:1;
+                    sorted_pValues_bonf      = min(1, sorted_data.*factors);
+                    sorted_pValues_bonf(2:num_of_samples) = max([sorted_pValues_bonf(1:num_of_samples-1); sorted_pValues_bonf(2:num_of_samples)]);
+                    stat_data.contrasts.pValues_bonf.(['contrast_' num2str(contrast)])  = sorted_pValues_bonf(real_ids);
+                   
+                    num_of_samples         = length(total_data.x_axis);
+                    data2save.bin          = (1:num_of_samples)';
+                    data2save.x_axis       = total_data.x_axis';
+                    data2save.tValues      = stat_data.contrasts.tValues.(['contrast_' num2str(contrast)])(1:num_of_samples)';
+                    data2save.pValues      = stat_data.contrasts.pValues.(['contrast_' num2str(contrast)])(1:num_of_samples)';
+                    data2save.pValues_bonf = stat_data.contrasts.pValues_bonf.(['contrast_' num2str(contrast)])(1:num_of_samples)';
+                    data2save.BFs          = stat_data.contrasts.BFs.(['contrast_' num2str(contrast)])(1:num_of_samples)';
+                    contrast_names_fixed   = strrep(strrep(strrep(contrast_name, 'c_', ''), '_x_', ' & '),'_',' ');
+                    writetable(struct2table(data2save), [total_data.paths.stat_output_folder_name filesep contrast_names_fixed '.csv'])
+
                 else
-                    stat_data.contrasts.tValues = statistical_data.contrasts.tValues;
-                    stat_data.contrasts.BFs     = statistical_data.contrasts.BFs;
-                    stat_data.contrasts.pes     = statistical_data.contrasts.pes;
-                end;
+                    stat_data.contrasts.tValues      = statistical_data.contrasts.tValues;
+                    stat_data.contrasts.pValues      = statistical_data.contrasts.pValues;
+                    stat_data.contrasts.pValues_bonf = statistical_data.contrasts.pValues_bonf;
+                    stat_data.contrasts.BFs          = statistical_data.contrasts.BFs;
+                    stat_data.contrasts.pes          = statistical_data.contrasts.pes;
+                end
+                
+                
                 stat_data.contrasts.total.bf(contrast, :) = total_bf;
                 stat_data.contrasts.total.t(contrast, :)  = total_t;
                 stat_data.contrasts.total.N(contrast, :) = N;
                 stat_data.contrasts.total.cohensd(contrast, :) = (nanmean(total_avg1)-nanmean(total_avg2))/total_sd;
-            end;
+            end
             all_fixed_contrast_names = cell(size(contrasts, 1), 1);
             for contrast = 1:size(contrasts, 1)
                 all_fixed_contrast_names{contrast, :} = [char(comp_names_fixed(contrasts(contrast, 1))), ' - ', char(comp_names_fixed(contrasts(contrast, 2)))];
@@ -440,6 +495,6 @@ classdef stat
               stat_data.contrasts.total.cohensd,...
               'VariableNames', {'Comperison', 't', 'BF','N', 'cohensd'});
             stat_data.contrast_names = all_fixed_contrast_names;
-        end; 
+        end 
     end
 end
